ba621b01ce969250d9e891aca05ab188d4e9e2aa
Jay Williams <course@jaywilliams.me>
2018-02-08 20:21:17 +0000

    Remove gitupdate command while I'm docker hosting

--- main.js
+++ main.js
@@ -5,58 +5,58 @@ const commands = require("./commands");
 const user = require("./user");
 const matchmaker = require("./matchmaker");
 const exec = require('child_process')
-  .exec;
+    .exec;
 const bot = new Discord.Client();
 var botConfig = JSON.parse(fs.readFileSync('config/config.json', 'utf-8'));
 var botToken = botConfig.bot_token;
 var riotApiKey = botConfig.riot_api_key;
 var onReady = false;
 if (process.platform === "win32") {
-  var rl = require("readline")
-    .createInterface({
-      input: process.stdin,
-      output: process.stdout
+    var rl = require("readline")
+        .createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    rl.on("SIGINT", function() {
+        process.emit("SIGINT");
     });
-  rl.on("SIGINT", function () {
-    process.emit("SIGINT");
-  });
 }
-process.on("SIGINT", function () {
-  console.log("Disconnecting bot..")
-  saveAll();
-  process.exit();
+process.on("SIGINT", function() {
+    console.log("Disconnecting bot..")
+    saveAll();
+    process.exit();
 });
 
 function saveAll() {
-  behaviour.save();
-  user.save();
-  bot.destroy();
+    behaviour.save();
+    user.save();
+    bot.destroy();
 }
 bot.on("ready", () => {
-  if (onReady) {
-    console.log("Reconnected!");
-    return;
-  }
-  console.log('Connected!');
-  console.log("Bot name: " + bot.user.username);
-  console.log("Bot id: " + bot.user.id);
-  behaviour.load();
-  commands.load();
-  user.load(riotApiKey);
-  matchmaker.load();
+    if (onReady) {
+        console.log("Reconnected!");
+        return;
+    }
+    console.log('Connected!');
+    console.log("Bot name: " + bot.user.username);
+    console.log("Bot id: " + bot.user.id);
+    behaviour.load();
+    commands.load();
+    user.load(riotApiKey);
+    matchmaker.load();
 
-  commands.reg("%update", require("./helpers/gitUpdate"), true, "Update the bot");
-  onReady = true;
+    // commands.reg("%update", require("./helpers/gitUpdate"), true, "Update the bot");
+    onReady = true;
 });
 bot.on('message', msg => {
-  // @TODO Don't process if the message is from a bot
+    // @TODO Don't process if the message is from a bot
 
-  // @Cleanup just a quick command to check if the bot is online
-  if (msg.content == "%ping") {
-    msg.channel.send("Pong!");
-    return;
-  }
-  
-  commands.process(msg);
+    // @Cleanup just a quick command to check if the bot is online
+    if (msg.content == "%ping") {
+        msg.channel.send("Pong!");
+        return;
+    }
+
+    commands.process(msg);
 });
 bot.login(botToken);
